60d9d3d0c32643b998d815304c83bb0aea88eed3
alfredks <alfredks@WS-VGY-1941>
2016-09-16 13:55:38 +0200

    Add project files.

---
+++ forOvningar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forOvningar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
